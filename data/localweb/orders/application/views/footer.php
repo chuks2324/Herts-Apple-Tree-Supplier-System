ed75b1da641ca19489561fb3b4c365be23650160
chuks2324 <54580449+chuks2324@users.noreply.github.com>
2022-06-06 00:09:44 +0100

    database inserts

---
+++ data/localweb/orders/application/views/footer.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8" />
+	<link rel="stylesheet" href="assets/style/bootstrap.css">
+	<!--
+	<style>
+	#nav { font-family: Arial; font-size: 14px; width: 100%; float: left; margin: 0 0 1em 0; padding: 0; list-style: none;}
+	#nav {list-style: none; border:0;}
+	#rightnav { list-style: none; }
+	#nav li { float: left; }
+	#rightnav li { float: right; }
+	#nav li a { margin: 0 3px 0 0; font-size: 15px; display: block; padding: 8px 15px; text-decoration: none; color: #000; background-color: #f2f2f2; border: 1px solid #c1c1c1;}
+	#nav li a:hover {background-color: #f2e4d5;}
+	#nav a:link, a:visited {border-radius: 12px 12px 12px 12px; }		
+	</style>
+	-->
+</head>
+<body>
+	<footer>
+		<div>
+			
+		</div>
+</footer>
+	
+</body>
+</html>
--- data/localweb/orders/application/views/header.php
+++ data/localweb/orders/application/views/header.php
@@ -2,6 +2,8 @@
 <html>
 <head>
 	<meta charset="utf-8" />
+	<link rel="stylesheet" href="/orders/assets/grocery_crud/css/custom.css">
+	<!--
 	<style>
 	#nav { font-family: Arial; font-size: 14px; width: 100%; float: left; margin: 0 0 1em 0; padding: 0; list-style: none;}
 	#nav {list-style: none; border:0;}
@@ -12,20 +14,28 @@
 	#nav li a:hover {background-color: #f2e4d5;}
 	#nav a:link, a:visited {border-radius: 12px 12px 12px 12px; }		
 	</style>
+	-->
 </head>
 <body>
-	<div>
-		<ul id="nav">
-		<li><a href='<?php echo site_url('')?>'>Home</a></li>
-		<li><a href='<?php echo site_url('main/orders')?>'>Orders</a></li>
-		<li><a href='<?php echo site_url('main/items')?>'>Items</a></li>
-		<li><a href='<?php echo site_url('main/customers')?>'>Customers</a></li>
-		<li><a href='<?php echo site_url('main/orderline')?>'>Order Line</a></li>
-			<ul id="rightnav">
-			<li><a href='<?php echo site_url('main/blank')?>'>Blank Page</a></li>
-			<li><a href='<?php echo site_url('main/querynav')?>'>Queries</a></li>
+	<header>
+		<div id="top">
+			<img id="logo" src="/orders/assets/grocery_crud/css/ui/simple/images/logo.jpg" alt="logo">
+			<h1> Herts Apple Tree Management System</h1>
+		</div>
+		<div class="navbar">
+			<ul class="nav">
+				<li><a class="nav-link" href='<?php echo site_url('')?>'>Home</a></li>
+				<li><a class="nav-link" href='<?php echo site_url('main/orders')?>'>Orders</a></li>
+				<li><a class="nav-link" href='<?php echo site_url('main/items')?>'>Items</a></li>
+				<li><a class="nav-link" href='<?php echo site_url('main/customers')?>'>Customers</a></li>
+				<li><a class="nav-link" href='<?php echo site_url('main/orderline')?>'>Order Line</a></li>
+					<ul class="nav">
+						<li><a class="nav-link" href='<?php echo site_url('main/blank')?>'>Blank Page</a></li>
+						<li><a class="nav-link" href='<?php echo site_url('main/querynav')?>'>Queries</a></li>
+					</ul>
 			</ul>
-		</ul>
-	</div>
+		</div>
+	</header>
+	
 </body>
 </html>
